6326e36ee8b2fff20fde21ccc5c6644b8609326c
zsjon <cho010105@naver.com>
2025-07-16 14:52:15 +0900

    feature: 관리자/마스터 로그인 form 구분 1. usePathname을 활용하여 /admin/login이면 관리자 로그인, /master/login이면 마스터 로그인 페이지로 이동 2. DummyCompanyData.tsx를 생성하여 기업명 입력 시 db 대신 더미데이터로 기업명 조회화도록 검색 드롭다운 구현 3. Master와 Admin 경로 구분

--- src/components/Login/LoginForm/LoginForm.tsx
+++ src/components/Login/LoginForm/LoginForm.tsx
@@ -1,24 +1,85 @@
 // 로그인 페이지용 Form입니다.
 'use client';
 
-import {useState} from 'react';
+import React, {useState} from 'react';
+import {usePathname} from 'next/navigation';
 import LoginButton from '@/components/Login/Buttons/LoginButton';
 import './LoginForm.scss';
+import {dummyCompanies} from "@/constants/dummydata/DummyCompanyData";
 
 export default function LoginForm() {
+    const pathname = usePathname();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [companyName, setCompanyName] = useState('');
+
+    // dummydata 기반 임시 기업명 검색용 변수
+    const [suggestions, setSuggestions] = useState<string[]>([]);
+    const [showSuggestions, setShowSuggestions] = useState(false);
+
+    //
+    const handleCompanyInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const input = e.target.value;
+        setCompanyName(input);
+
+        if (input.trim() === '') {
+            setSuggestions([]);
+            setShowSuggestions(false);
+            return;
+        }
+
+        const filtered = dummyCompanies.filter((company:string) => company.toLowerCase().includes(input.toLowerCase()));
+        setSuggestions(filtered);
+        setShowSuggestions(true);
+    }
+
+    const handleSelectSuggestion = (company: string) => {
+        setCompanyName(company);
+        setSuggestions([]);
+        setShowSuggestions(false);
+    }
+
 
     const handleLogin = () => {
-        console.log('로그인 시도:', {email, password});
-        // 여기에 실제 로그인 로직 추가
+        console.log('로그인 시도:', {companyName, email, password});
+        // 여기에 실제 로그인 로직 추가하기
     };
+
 
     return (
         <>
             <form className="login-form">
+                {pathname === '/master/login' && (
+                    <label className="login-form-label">
+                        <p className="login-form-description">기업명</p>
+                        <input
+                            id="companyName"
+                            name="companyName"
+                            type="companyName"
+                            placeholder="이메일을 입력해 주세요."
+                            value={companyName}
+                            className="login-form-input"
+                            // onChange={(e) => setCompanyName(e.target.value)}/>
+                            onChange={handleCompanyInputChange}
+                        />
+                        {showSuggestions && suggestions.length > 0 && (
+                            <ul className="company-suggestion-dropdown">
+                                {suggestions.map((s, i) => (
+                                    <li
+                                        key={i}
+                                        className="suggestion-item"
+                                        onClick={() => handleSelectSuggestion(s)}
+                                    >
+                                        {s}
+                                    </li>
+                                ))}
+                            </ul>
+                            )}
+                    </label>
+                )}
+
                 <label className="login-form-label">
-                    E-Mail
+                    <p className="login-form-description">E-Mail</p>
                     <input
                         id="email"
                         name="email"
