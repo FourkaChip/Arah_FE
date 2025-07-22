6be9b5d0476296876d243967c7cddfe80ce333b0
Kim-songhui <rlathdgmlkk@naver.com>
2025-07-23 01:00:13 +0900

    fix: cdn링크 위치 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,11 +8,13 @@ import type { ReactNode } from 'react';
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
-      <body>
-        <link
+      <head>
+      <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         />
+      </head>
+      <body>
         {children}
       </body>
     </html>
