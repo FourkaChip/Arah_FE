4eea607cfa7964ae5485a0fba69014688ff98735
zsjon <cho010105@naver.com>
2025-07-24 09:48:24 +0900

    fix: CustomSearch 수정 1. CustomSearch url 분기별 placeholder 변경

--- src/components/CustomSearch/CustomSearch.tsx
+++ src/components/CustomSearch/CustomSearch.tsx
@@ -1,14 +1,27 @@
 // Datagrid와의 연동을 통해 검색 기능을 구현하기 위한 커스텀 검색 input 컴포넌트입니다.
 // import "@/styles/globals.scss";
 import "./CustomSearch.scss";
-import { useState } from "react";
+import {useState} from "react";
+import {usePathname} from "next/navigation";
 
 interface CustomSearchProps {
     onSearch: (value: string) => void;
 }
 
-export default function CustomSearch({ onSearch, className = "" }: CustomSearchProps & { className?: string }) {
+export default function CustomSearch({onSearch, className = ""}: CustomSearchProps & { className?: string }) {
     const [input, setInput] = useState("");
+    const pathname = usePathname();
+
+    const getPlaceholder = () => {
+        switch (pathname) {
+            case '/master/managers':
+                return "검색할 관리자를 입력해 주세요.";
+            case '/master/dept':
+                return "검색할 부서를 입력해 주세요.";
+            default:
+                return "검색어를 입력해 주세요.";
+        }
+    };
 
     const handleSearch = () => {
         onSearch(input);
@@ -18,7 +31,7 @@ export default function CustomSearch({ onSearch, className = "" }: CustomSearchP
         <div className={`admin-search-input-wrapper ${className}`}>
             <input
                 className="admin-search-input"
-                placeholder="검색할 관리자를 입력해 주세요."
+                placeholder={getPlaceholder()}
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
                 onKeyDown={(e) => {
@@ -30,7 +43,7 @@ export default function CustomSearch({ onSearch, className = "" }: CustomSearchP
                 onClick={handleSearch}
                 aria-label="검색"
             >
-                <i className="fas fa-search" />
+                <i className="fas fa-search"/>
             </button>
         </div>
     );
