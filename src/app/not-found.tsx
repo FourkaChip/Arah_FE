2818675a75ad39a20af4dd105582d6ac492cf2db
zsjon <cho010105@naver.com>
2025-08-01 13:28:58 +0900

    design: 404 페이지 디자인 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,15 @@
+// 페이지가 존재하지 않는 라우트 경로에 대한 컴포넌트입니다.
+import './not-found.scss';
+import NotFoundClient from './not-found.client';
+
 export default function NotFound() {
-    return <div>
-        <h1>Not Found</h1>
-        <h2>There is no route here.</h2>
-        <button className="button is-primary">Return to Home</button>
-    </div>
+    return (
+        <div className="not-found-container is-flex is-align-items-center is-justify-content-center">
+            <div className="box has-text-centered" style={{minWidth: 340, maxWidth: 420}}>
+                <h1 className="not-found-title title is-1 has-text-primary">404 NOT FOUND</h1>
+                <h2 className="not-found-desc subtitle is-5">존재하지 않는 페이지입니다.</h2>
+                <NotFoundClient/>
+            </div>
+        </div>
+    );
 }
