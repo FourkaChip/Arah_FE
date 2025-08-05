9ddf2af6a4f12858bd5eb5dcbdd59a08157a8674
Kim-songhui <rlathdgmlkk@naver.com>
2025-08-05 16:35:20 +0900

    refactor: 파일명 변경 및 확장자 변경

---
+++ src/constants/modalConfig.ts
@@ -0,0 +1,14 @@
+export const modalInputFilledConfig = {
+    'password-lost': {
+        title: '비밀번호 분실',
+        description: '비밀번호를 분실하셨나요? 담당 관리자에게 문의하세요.',
+        value: 'fourkachip@gmail.com',
+        buttonLabel: '확인',
+    },
+    'token-check': {
+        title: '기업 토큰 조회',
+        description: '카카오워크 내 그룹 채팅방을 생성할 수 있는 토큰을 확인합니다.',
+        value: '',
+        buttonLabel: '재등록',
+    },
+} as const;
